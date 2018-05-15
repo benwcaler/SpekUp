14f30fc9845c3b60ddff1c35b7c1ee7833719432
mpras55 <manjunath.bp@gmail.com>
2018-05-14 17:45:45 -0700

    Fixed current and updated timestamp

--- models/users.js
+++ models/users.js
@@ -7,7 +7,16 @@ module.exports = function (sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false,
 			unique: true
-		},
+		},'createdAt': {
+      type: DataTypes.DATE(3),
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3)'),
+    },
+    'updatedAt': {
+      type: DataTypes.DATE(3),
+      defaultValue: sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'),
+    },
+  }, {
+    timestamps: true
 	});
 
 	User.associate = function (models) {
