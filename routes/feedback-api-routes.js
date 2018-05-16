34c74dbde83b914b30602bc0224d84fa338996d4
mpras55 <manjunath.bp@gmail.com>
2018-05-16 00:14:57 -0700

    Polls, Poll_Data & Feedback APIs

--- routes/feedback-api-routes.js
+++ routes/feedback-api-routes.js
@@ -1,5 +1,19 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
+	app.post("/api/feedback", function (req, res) {
+		console.log(req.body);
+		db.Feedback.create({
+			feedback_type: req.body.feedback_type
+			, feedback_text: req.body.feedback_text
+			, EventId: req.body.EventId
+		})
+			.then(function (dbFeedback) {
+				res.json(dbFeedback);
+			})
+			.catch(function (error) {
+				console.log("Invalid data for insert");
+				res.json(error);
+			});
+	});
 };
